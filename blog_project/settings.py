ccb8404301273a2778cf68130208d8ea361d03c6
ravin <pandeyravinder078@gmail.com>
2025-08-06 09:07:21 +0530

    changes in setting .py

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['my-blog-p7oz.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
